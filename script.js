b534fc74c1cefff71267742b30b46d2eff833a50
jackie <jchoe2@binghamton.edu>
2023-01-24 00:01:14 -0500

    hi

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+
+for(var i = 0; len = document.length; i++){
+    for(var j = 0; len = letters.length; j++){
+        texts[j].style.visibility=visible;
+    }
+}
